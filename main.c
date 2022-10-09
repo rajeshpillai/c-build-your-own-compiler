49afff9b353995c432b1ddbe6a117061fda94b55
rajeshpillai <pillai.rajesh@gmail.com>
2022-10-09 21:23:18 +0530

    Vector helper example

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include "helpers/vector.h"
+
 int main() {
-  printf("Hello World!\n");
+  struct vector* vec = vector_create(sizeof(int));
+  int x = 50;
+
+  vector_push(vec, &x);
+  x = 20;
+
+  vector_push(vec, &x);
+
+  // pop off the 20
+  vector_pop(vec);
+
+  // Peek into vector
+  vector_set_peek_pointer(vec, 0);
+
+  int* ptr = vector_peek(vec);  // returns value push to stack
+
+  while(ptr) {
+    printf("%i\n", *ptr);
+    ptr = vector_peek(vec);
+  }
+  
   return 0;
 }  
